0b4b30cc3fac2e026f017089f612f54cea0f67ca
Tejaswini Vaddemgunta <tejaswini.v317@gmail.com>
2025-06-01 23:26:48 +0530

    Update and rename solution.java to Solution.java

---
+++ Solutions/Solution.java
@@ -0,0 +1,28 @@
+package Solutions;
+
+public class Solution {
+    public List<String> letterCasePermutation(String s) {
+        List<String> result = new ArrayList<>();
+        backtrack(s.toCharArray(), 0, result);
+        return result;
+    }
+
+    private void backtrack(char[] chars, int index, List<String> result) {
+        if (index == chars.length) {
+            result.add(new String(chars));
+            return;
+        }
+
+        if (Character.isLetter(chars[index])) {
+            // Lowercase branch
+            chars[index] = Character.toLowerCase(chars[index]);
+            backtrack(chars, index + 1, result);
+
+            // Uppercase branch
+            chars[index] = Character.toUpperCase(chars[index]);
+            backtrack(chars, index + 1, result);
+        } else {
+            backtrack(chars, index + 1, result);
+        }
+    }
+}
